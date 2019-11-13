5dfab1f43284e034baf4b54582a1e1a4a711e13f
Brooke Bachman <bbachman1994@gmail.com>
2019-11-13 19:51:24 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -65,6 +65,7 @@ def gross_per_studio(collection)
   collection.each do |movie|
   new_hash +=  movie[:worldwide_gross]
   binding.pry
+  end
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
   # return a Hash that includes the total worldwide_gross of all the movies from
   # each studio.
